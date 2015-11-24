3b97397ba7d8a9cf54fff1c5b0a154eae5fe2729
julian <jutanke@googlemail.com>
2015-11-24 22:57:54 +0100

    make paint always use the new mat

--- zmq_tracker.py
+++ zmq_tracker.py
@@ -1,14 +1,22 @@
 import biotracker
 
+Mat = None
+
 
 def track(frame, M):
+    global Mat
+    Mat = M
     print("track " + str(frame) + str(M.shape))
 
 
 def paint(qpainter, frame):
+    global Mat
     print("paint " + str(frame))
     qpainter.setPen((255, 0, 0, 255))
     qpainter.drawRect((20, 20, 60, 60))
+    if Mat is not None:
+        return Mat
+
 
 
 def shutdown():
