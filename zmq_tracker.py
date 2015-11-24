3dd508af58f28c4859bb000173089f584c3f2e3b
julian <jutanke@googlemail.com>
2015-11-24 17:43:16 +0100

    update sample tracker

--- zmq_tracker.py
+++ zmq_tracker.py
@@ -1,34 +1,15 @@
 import biotracker
-import numpy as np
-
-print("start")
-
-print("start zmq client")
-
-x = 0
-y = 1
-w = 1
-h = 1
 
 
 def track(frame, M):
-    print("track")
+    print("track " + str(frame))
 
 
 def paint(frame):
-    print("paint")
+    print("paint " + str(frame))
 
-#while True:
-#    print("wait for next mat")
-#    M = biotracker.recv_mat()
-#
-#    N = np.zeros_like(M)
-#    N[(M >= 200).nonzero()] = 255
-#
-#    print("matrix:" + str(M[400][402]))
-#
-#    p = biotracker.QPainter()
-#    p.setPen((0, 0, 255, 255))
-#    p.drawRect((x, y, w, h))
-#
-#    biotracker.send_painter(p)
+
+def shutdown():
+    print("shutdown")
+
+biotracker.run_client(track, paint, shutdown)
