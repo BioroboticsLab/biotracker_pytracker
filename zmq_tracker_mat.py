1c2149e20b82e645933bc7609c88dfc0307733f5
julian <jutanke@googlemail.com>
2016-01-14 16:01:15 +0100

    remove Signals in the context of returning the list of widgets. The behavior here should be changed at some point to allow signals at this place (widget request) as well.

--- zmq_tracker_mat.py
+++ zmq_tracker_mat.py
@@ -2,7 +2,7 @@ from biotracker import (
     Signals,
     Button,
     Text,
-    run_client )
+    run_client)
 import numpy as np
 
 Mat = None
@@ -13,38 +13,41 @@ def track(frame, M):
     Mat = np.absolute(M - 50) 
     Signals.notify_gui("track" + str(frame) + str(Mat.shape))
 
+
 def paint(frame):
     global Mat
     Signals.notify_gui("paint")
     if Mat is not None:
         return Mat
 
+
 def paint_overlay(qpainter):
     Signals.notify_gui("paint overlay")
     qpainter.setPen(255, 255, 0, 255)
     qpainter.drawRect((20, 20, 60, 60))
 
+
 def shutdown():
     Signals.notify_gui("shutdown")
+
 
 def btn_click():
     Signals.notify_gui("button click")
 
+
 def btn2_click():
     Signals.notify_gui("button2 click")
 
+
 def request_widgets():
-    Signals.notify_gui("request widgets btn")
     return [
         Button("ClickMe", btn_click),
         Button("ClickMeToo", btn2_click),
-	Text("Funny text:")
-    ]
-
+        Text("Funny text:")]
 
 run_client(
     track,
     paint,
     paint_overlay,
     shutdown,
-    request_widgets=request_widgets )
+    request_widgets=request_widgets)
