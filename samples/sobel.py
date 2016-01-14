44b2d875b46bfd63eec2420ba4e64b2ea3f1c046
justayak <julian.tanke@googlemail.com>
2016-01-14 15:55:35 +0000

    rgb2gray

--- samples/sobel.py
+++ samples/sobel.py
@@ -4,7 +4,7 @@ from biotracker import (
     run_client
 )
 import numpy as np
-from scipy import signal, misc
+from scipy import signal, ndimage
 
 Mat = None
 Kx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
@@ -12,33 +12,12 @@ Ky = Kx.T
 show_x = True
 
 
-def average(px):
-    """
-    Helper for converting to grayscale
-    :return:
-    """
-    return 0.299 * px[0] + 0.587 * px[1] + 0.114 * px[2]
-
-def to_gray(M):
-    """
-    convert image to grayscale
-    :param M:
-    :return:
-    """
-    gray = np.zeros((M.shape[0], M.shape[1]))
-    for row in range(len(M)):
-        for col in range(len(M[row])):
-            gray[row][col] = average(M[row][col])
-    return gray
-
-
 def track(frame, M):
     global Mat, Kx, Ky, show_x
-    M = to_gray(M)
     if show_x:
-        Mat = signal.convolve2d(M, Kx, boundary='symm', mode='same')
+        Mat = ndimage.sobel(M, 0)
     else:
-        Mat = signal.convolve2d(M, Ky, boundary='symm', mode='same')
+        Mat = ndimage.sobel(M, 1)
 
 
 def paint(frame):
